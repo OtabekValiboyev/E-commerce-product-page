b1d391a350002cd9186fd7bcee0c0cd5ebacefa8
25-Otabek <valiboyevo@gmail.com>
2023-09-20 17:00:00 +0500

    Add thumbnail image click to js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const modifiers = {
+  ImgThumbnailActive: 'img-showcase__thumbnail--active'
+};
+
+// HEADER CARD
 const elHeaderCardLink = document.querySelector('.js-header__cart-link');
 const elHeaderModal = document.querySelector('.header__modal');
 
@@ -5,4 +10,27 @@ if (elHeaderCardLink) {
   elHeaderCardLink.addEventListener('click', function () {
     elHeaderModal.classList.toggle('header__modal--open')
   });
-}
+};
+
+// SITE SHOWCASE
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__active-img');
+const elsImgShowcaseThumbnailButtons = document.querySelectorAll('.js-img-showcase-thumbnail-button');
+const elsImgShowcaseThumbnail = document.querySelectorAll('.js-img-showcase-thumbnail');
+
+
+elsImgShowcaseThumbnailButtons.forEach(function (elImgButton) {
+  elImgButton.addEventListener('click', function () {
+
+    elsImgShowcaseThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifiers.ImgThumbnailActive);
+    });
+
+    elImgButton.parentElement.classList.add(modifiers.ImgThumbnailActive);
+
+    elImgShowcaseActiveImg.src = elImgButton.dataset.imgShowcaseBig;
+    elImgShowcaseActiveImg.srcset = `${elImgButton.dataset.imgShowcaseBig} 1x, ${elImgButton.dataset.imgShowcaseRetina} 2x`;
+
+    // console.log(elImgButton.dataset.imgShowcaseBig);
+    // console.log(elImgButton.dataset.imgShowcaseRetina);
+  });
+});
